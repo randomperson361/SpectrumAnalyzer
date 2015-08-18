36d73c5098fed84bfc74ec1877df4548aff1f5af
Andrew McNeill <mcneill.andrew.d@gmail.com>
2015-08-17 23:09:12 -0500

    changed audio lib ADC reference from INTERNAL 1.2v to DEFAULT 3.3v

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-08-17 23:00:45
+//This file has been generated on 2015-08-17 23:08:23
 
 #include "Arduino.h"
 void setup() ;
